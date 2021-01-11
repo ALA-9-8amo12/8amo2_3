c814e924f79422235c19d81bb058914f3abfd4e3
CanEnes8amo2 <1039335@mborijnland.nl>
2021-01-11 08:43:04 +0100

    pushing latest version of oefen pagina

--- app/src/main/java/com/example/amazighdroid/oefenitem.java
+++ app/src/main/java/com/example/amazighdroid/oefenitem.java
@@ -1,12 +1,24 @@
 package com.example.amazighdroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.StorageReference;
+
 public class oefenitem extends AppCompatActivity {
+    private static final String TAG = "OefenItem";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,22 @@ public class oefenitem extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView3);
         textView.setText(selectedValue);
+
+        db.collection("Dieren_01")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+
     }
 }
