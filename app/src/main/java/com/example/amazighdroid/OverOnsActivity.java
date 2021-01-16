c9c572d2431dee7631c1c0bab4e73cfe0d99f8ab
VerboonMatthijs8amo2 <1042849@mborijnland.nl>
2021-01-16 15:55:49 +0100

    over ons pagina

---
+++ app/src/main/java/com/example/amazighdroid/OverOnsActivity.java
@@ -0,0 +1,15 @@
+package com.example.amazighdroid;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OverOnsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_over_ons);
+    }
+}
