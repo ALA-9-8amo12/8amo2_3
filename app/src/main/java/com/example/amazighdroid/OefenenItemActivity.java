ea32312554569f278baaf8e835ea8c650d3e5616
CanEnes8amo2 <1039335@mborijnland.nl>
2021-01-16 16:24:20 +0100

    removed unneeded imports

--- app/src/main/java/com/example/amazighdroid/OefenenItemActivity.java
+++ app/src/main/java/com/example/amazighdroid/OefenenItemActivity.java
@@ -6,9 +6,6 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,9 +14,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class OefenenItemActivity extends AppCompatActivity {
 
