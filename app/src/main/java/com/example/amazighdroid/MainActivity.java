0c6a2ddbab7e31db520c46c6494c458da446cd7b
CanEnes8amo2 <1039335@mborijnland.nl>
2021-01-16 16:34:12 +0100

    fixed activity not opening

--- app/src/main/java/com/example/amazighdroid/MainActivity.java
+++ app/src/main/java/com/example/amazighdroid/MainActivity.java
@@ -7,25 +7,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = (Button)findViewById(R.id.button6);
-        Button btn2 = (Button)findViewById(R.id.button7);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+
+        Button oefenen = (Button) findViewById(R.id.button6);
+        oefenen.setOnClickListener(this); // calling onClick() method
+
+        Button over_ons = (Button) findViewById(R.id.button7);
+        over_ons.setOnClickListener(this); // calling onClick() method
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button6:
                 startActivity(new Intent(MainActivity.this, OefenenActivity.class));
-            }
-        });
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.button7:
                 startActivity(new Intent(MainActivity.this, OverOnsActivity.class));
-            }
-        });
+                break;
+            default:
+                break;
+        }
     }
 }
